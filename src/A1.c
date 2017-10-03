0105e721e89440af630482eea69a7e79d5dd830b
alexH2456 <alexander.harris@mail.mcgill.ca>
2017-10-02 22:35:19 -0400

    Command parser finished, requires debugging

--- src/A1.c
+++ src/A1.c
@@ -1,12 +1,72 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
+#include <time.h>
+#include <string.h>
+#include <signal.h>
 
-int main()
+// Alexander Harris - 260688155
+//
+//TODO: Set up child process creation using fork() and waitpid(), write execvp function for ls/pwd/cd/cat/cp/fg/jobs/exit and background(&), 
+
+int getcmd(char *prompt, char *args[], int *background)
 {
-	close(1);
-	open("redirect.txt", O_WRONLY | O_TRUNC | O_CREAT | O_EXCL);
-	printf("Hello World.\n");
+	int length, i = 0;
+	char *token, *loc;
+	char *line = NULL;
+	size_t linecap = 0;
+	printf("%s", prompt);
+	length = getline(&line, &linecap, stdin);
+	if(length <= 0){
+		exit(-1);
+	}
 
-	return 0;
+	if((loc = index(line, '&')) != NULL){
+		*background = 1;
+		*loc = ' ';
+	}
+	else{
+		*background = 0;
+	}
+
+	while((token = strsep(&line, " \t\n")) != NULL){
+		for(int j = 0; j < strlen(token); j++){
+			if(token[j] <= 32){
+				token[j] = '\0';
+			}
+		}
+		if(strlen(token) > 0){
+			args[i++] = token;
+		}
+		return i;
+	}
+}
+
+int main(void)
+{
+	time_t now;
+	srand((unsigned int) (time(&now)));
+
+	char *args[20];
+	int bg;
+	while(1){
+		bg = 0;
+		memset(args, 0, sizeof(args));
+		int cnt = getcmd("\n>> ", args, &bg);
+		
+		//Increases execution time for syscalls
+		/*int w, rem;
+		w =  rand() % 10;
+		rem = sleep(w);
+		while(rem != 0){
+			rem = sleep(rem);
+		}*/
+
+		if(args[0] != NULL){
+			printf("%s\n", args[0]);
+		} 
+		else{
+			printf(" ");
+		}
+	}
 }
