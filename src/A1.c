7a740426f5d688937617955e2c475adf252f06dc
alexH2456 <alexander.harris@mail.mcgill.ca>
2017-10-05 21:35:38 -0400

    Finished as much as possible, fg still broken

--- src/A1.c
+++ src/A1.c
@@ -1,6 +1,7 @@
 /* 
 * Alexander Harris - 260688155
 * ESCSE 427 - Assignment 1
+* Fall 2017
 * Simple Shell
 */
 
@@ -12,51 +13,59 @@
 #include <signal.h>
 #include <fcntl.h>
 #include <sys/wait.h>
-#include <sys/types.h>
 
-#define MAX_ARGS 40
+#define MAX_ARGS 20
 
-//TODO: Set up output redirection, jobs and background function, sig handling.
+struct job *head_job = NULL;
+struct job *tail_job = NULL;
 
-int commandNumber = 0;
-struct node *head_job = NULL;
-struct node *current_job = NULL;
-int process_pid;
-
-struct node {   //Linked list object
+struct job {   //Linked list object for jobs function
     int number; 
     int pid;
-    struct node *next;
+    char* job;
+    struct job *next;
 };
 
-void addToJobList(char *args[]) {
+void addToJobList(char* args, int *pid)     //Adds a process to the job linked list
+{
+    struct job *job = malloc(sizeof(struct job));
 
-    struct node *job = malloc(sizeof(struct node));
-
-    //If the job list is empty, create a new head
     if (head_job == NULL) {
         job->number = 1;
-        job->pid = process_pid;
-
-        //the new head is also the current node
+        job->pid = *pid;
+        job->job = args;
         job->next = NULL;
         head_job = job;
-        current_job = head_job;
+        tail_job = head_job;
     }
-
-    //Otherwise create a new job node and link the current node to it
     else {
 
-        job->number = current_job->number + 1;
-        job->pid = process_pid;
-
-        current_job->next = job;
-        current_job = job;
+        job->number = tail_job->number + 1;
+        job->pid = *pid;
+        job->job = args;
         job->next = NULL;
+        tail_job->next = job;
+        tail_job = job;
+        
     }
 }
 
-int getcmd(char *prompt, char *args[], int *background)
+int findJob(int number)     //Finds a specified process number in the list and returns the pid
+{
+    struct job *running = head_job;
+
+    while(running != NULL){
+        if(running->number == number){
+
+            return running->pid;
+        }
+
+        running = running->next;
+    }
+    return -1;
+}
+
+int getcmd(char *prompt, char *args[], int *background)     //Parses shell input for commands and arguments
 {
     int length, i = 0;
     char *token, *loc;
@@ -65,7 +74,7 @@ int getcmd(char *prompt, char *args[], int *background)
 
     //Resets args array
     for (int j = 0; j < MAX_ARGS; j++){
-        args[j] = '\0';
+        args[j] = NULL;
     }
  
     printf("%s", prompt);
@@ -74,7 +83,6 @@ int getcmd(char *prompt, char *args[], int *background)
     if(length <= 0){
         exit(-1);
     }
-
     if((loc = index(line, '&')) != NULL){
         *background = 1;
         *loc = ' ';
@@ -93,23 +101,40 @@ int getcmd(char *prompt, char *args[], int *background)
             args[i++] = token;
         }
     }
+
     free(line);
 
     return i;
 }
 
-static void sigHandler(int sig)
+void sigHandler()   //Deals with CTRL+C and CTRL+Z interrupts
 {
-    printf("Caught signal: %d\n", sig);
+    signal(SIGINT, sigHandler);
+    fflush(stdout);
+    exit(-1);
 }
 
-void exec(char* args[], int bg)
+void slowProcess(int sec)   //Slows process execution for testing purposes
 {
-    pid_t pid;
+    time_t now;
+    srand((unsigned int) (time(&now)));
+
+    int w, rem;
+    w =  rand() % sec;
+    rem = sleep(w);
+    while(rem != 0){
+        rem = sleep(rem);
+    }
+}
+
+void exec(char* args[], int bg)     //Method for execution of all built-in and other functions
+{
+    int pid;
     
     //Deals with '~' character for Home directory
     for(int i = 0; args[i]; i++){
         if(strchr(args[i], 126)){
+          
             char* home = getenv("HOME");
             char* new;
             char* copy = args[i];
@@ -123,46 +148,99 @@ void exec(char* args[], int bg)
                 args[i] = new;
             }
             else{
-                fprintf((stderr), "Mem alloc failed\n");
+                fprintf((stderr), "Memory allocation failed\n");
             }
         }
     }
 
+    //exit function
     if(strcmp(args[0], "exit") == 0){
         exit(0);
     }
+
+    //cd function
     else if(strcmp(args[0], "cd") == 0){
         int dir = chdir(args[1]);
+       
         if(dir != 0){
             printf("Invalid directory.\n");
         }
     }
 
-    //Use execvp for all other commands
+    //jobs function
+    else if(strcmp(args[0], "jobs") == 0){ 
+       
+        struct job *previous;
+        struct job *current = head_job;
+
+        while(current != NULL){
+            if(waitpid(current->pid, NULL, WNOHANG) == 0){
+                
+                printf("<%d> Running: %s Pid: %d\n", current->number, current->job, current->pid);
+                
+                previous = current;
+                current = current->next;
+            }
+            else{
+               
+                printf("<%d> Finished: %s Pid: %d\n", current->number, current->job, current->pid);
+               
+                head_job = current->next;
+                current = head_job;
+            }
+        }
+    }
+
+    //fg function (not working)
+    else if(strcmp(args[0], "fg") == 0){
+       
+        if(args[1] != NULL){
+            int jobNumber = atoi(args[1]);
+            pid = findJob(jobNumber);
+        }
+        if(pid == -1){
+            printf("Job does not exist\n");
+        }
+    }
+
+    //execvp for all other commands
     else{
         pid = fork();
 
         if(pid == 0){
+               
+            signal(SIGINT, sigHandler);     //CTRL+C kills child process
+
             //Output redirection
             for(int j = 0; args[j]; j++){
 
                 char* redirect = args[j+1];
 
-                if(strcmp(args[j], ">") == 0){
+                if(strcmp(args[j], ">") == 0){     //Overwrite
                     close(1);
                     open(redirect, O_WRONLY | O_CREAT | O_TRUNC, 0755);
+                    
                     args[j] = args[j+1] = NULL;
                 }
-                else if(strcmp(args[j], ">>") == 0){
+                else if(strcmp(args[j], ">>") == 0){    //Append
                     close(1);
                     open(redirect, O_WRONLY | O_CREAT | O_APPEND, 0755);
+                    
                     args[j] = args[j+1] = NULL;
                 }
             }
+            
+            //slowProcess(10);
             execvp(args[0], args);
         }
         else if(pid > 0){
-            waitpid(0, NULL, 0);
+
+            if(bg == 0){
+                waitpid(pid, NULL, WUNTRACED);
+            }
+            else{
+                addToJobList(args[0], &pid);
+            }
         }
         else{
             perror("Fork failed.\n");
@@ -173,32 +251,19 @@ void exec(char* args[], int bg)
 
 int main(void)
 {
-    time_t now;
-    srand((unsigned int) (time(&now)));
-
     char *args[MAX_ARGS];
     int bg;
 
-    if(signal(SIGINT, sigHandler) == SIG_ERR){
-        printf("Could not bind sig handler\n");
-        exit(1);
-    }
+    signal(SIGINT, SIG_IGN);
+    signal(SIGTSTP, SIG_IGN);
 
     while(1){
         bg = 0;
         int cnt = getcmd("\n>> ", args, &bg);
 
-        if(cnt <= 0){
+        if(cnt <= 0){ 
             continue;
         }
-        
-        //Increases execution time for syscalls
- /*       int w, rem;
-        w =  rand() % 10;
-        rem = sleep(w);
-        while(rem != 0){
-            rem = sleep(rem);
-        }*/
 
         exec(args, bg);
     }
